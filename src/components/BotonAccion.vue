4ab643f3b8ad280fc9af8bc08400104902da34e0
saidnahum <70162272+saidnahum@users.noreply.github.com>
2021-06-25 17:42:42 -0500

    first commit

---
+++ src/components/BotonAccion.vue
@@ -0,0 +1,20 @@
+<template>
+    <button>{{textoBoton}}</button>
+</template>
+
+<script>
+export default {
+    props: {
+        estado: Boolean
+    },
+    computed: {
+        textoBoton(){
+            return this.estado ? 'Aumentar' : 'Disminuir'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,23 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    contador: 120
   },
   mutations: {
+    incrementar(state){
+      state.contador = state.contador + 10
+    },
+    disminuir(state, payload){
+      state.contador = state.contador - payload
+    }
   },
   actions: {
+    accionIncrementar({commit}){
+      commit('incrementar')
+    },
+    accionDisminuir({commit}, numero){
+      commit('disminuir', numero)
+    }
   },
   modules: {
   }
